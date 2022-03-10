1242ff9f7109b7360e36da5b0ae64963ada8cb91
Chris <orj.tsuk@gmail.com>
2022-03-10 08:19:49 -0500

    setting up the basic structure. Pulled the correct data from the API.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from "./components/Card"
 
 function App() {
+
+  const [allHeroData, setAllHeroData] = React.useState([])
+  const [heroCards, setHeroCards] = React.useState([])
+
+
+
+  React.useEffect(() => {
+    const url = "https://api.opendota.com/api/heroStats"
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setAllHeroData(data))
+  }, [])
+
+  function getRandomHeroes() {
+    const pref = "http://cdn.dota2.com"
+    setHeroCards([])
+    for (let i = 0; i < 9; i += 1) {
+      let currHero = (Math.floor(Math.random() * 122))
+      console.log()
+      setHeroCards(old => ([...old,
+      {
+        heroName: allHeroData[currHero].localized_name,
+        heroPortrait: pref + allHeroData[currHero].img
+      }]))
+    }
+    console.log(heroCards)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getRandomHeroes}>Test</button>
+      <Card />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+//Have a card component, map over it? to display data.
+//display in random order everytime the user clicks one.
+
+//Scoreboard that increments when user clicks correct answer (not the same as the previous card)
+//Best score
---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = (props) => {
+    return(
+        <div>
+            Hello React
+        </div>
+    )
+}
+
+export default Card
